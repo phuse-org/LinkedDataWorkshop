671aaab69fa33fb169fa13652e725d98e96fb367
NovasTaylor <NovasTaylor@gmail.com>
2018-02-14 15:35:06 -0500

    script logic working.

--- CSS2018/scripts/R/ValidateTTLScript.R
+++ CSS2018/scripts/R/ValidateTTLScript.R
@@ -12,6 +12,7 @@
 #       Currently not checking that the proper prefixes were assigned!
 # TODO: 
 ###############################################################################
+library(reshape2) # melt
 library(redland)
 setwd("C:/_github/LinkedDataWorkshop/CSS2018")
        
@@ -70,7 +71,7 @@ ttlRelations <- sort(unique(ttlRelations))
 
 #---- Compare and flag values
 flaggedRelations <-setdiff(ttlRelations, standardRelations)
-flaggedRelations
+
 
 
 # ---- NODES
@@ -97,7 +98,9 @@ cleanNodes
 
 # Parsse Study node obtain attendee number used to check 
 #    other nodes: Person<n>, TrtArm<n-n>.
-studyNode <-cleanNodes[grepl("Study", cleanNodes)] 
+studyNode <-cleanNodes[grepl("(S|s)tudy", cleanNodes)] 
+
+#Get Attendee Num from Study Node. Must assume it is correct!
 attendeeNum <-gsub("eg:Study", "", studyNode)
 
 # Phase Node
@@ -105,40 +108,56 @@ phaseNode <-cleanNodes[grepl("Phase", cleanNodes)]
 
 #TODO Now use attendeeNum to check Person<n> and TrtArm<n> values.
 
+# Person Nodes
+personNodes <-cleanNodes[grepl("Person", cleanNodes)] 
+
+# TrtArm Nodes
+armNodes <-cleanNodes[grepl("TrtArm", cleanNodes)] 
+
 # Nodes that should be in all studies
 ttlNodes<-cleanNodes[!grepl("Study|Person|TrtArm|Phase", cleanNodes)] 
-ttlNodes
-
 
 flaggedNodes <- setdiff(ttlNodes, standardNodes)
-flaggedNodes
 
-
-# UNTESTED PSEUDO CODE BELOW HERE ---------------#
-
-# Check Nodes unique to each Attendee
-#Checks if nodes unique to each attendee. Append these to flaggedNodes if any.
-# Study : Pattern = Study<n> where <n> is any number. Cannot check the number
-#     for each attendee because that <n> is used AS THE ATTENDEE NUMBER in
-#     later entities. 
-if(grepl(attendeeNum, studyNode)){
-  print ("----Study pattern OK")
-}
-else{
-  append(flaggedNodes, studyNode)
-}
-
-
+# Check Nodes unique to each Attendee and flag any that do not fit required patter
+# Study<n> is NOT checked because Study<n> is used to extract the attendeeNum, so it
+#   will always be correct according to this code. 
 # Phase : Check that it is "Phase" then: Phase3, PhaseIIb, Phase2b all acceptable.
 if (grepl("ncit:Phase\\S{1-3}", phaseNode)) {
-  print ("Study Match")
+  print ("----Phase Pattern Match")
+}else{
+  print ("----ERROR: Phase Pattern fail.")
+   append(flaggedNodes, phaseNode)
 }
 
 # Person : Check : "Person" + "attendeeNum" + <n>
+personRegex <- paste0("eg:Person", attendeeNum, "\\d+")
+sapply(personNodes, function(person){
+  if (grepl(personRegex, person)) {
+    print ("----Person Pattern Match")
+  }else{
+    print ("----ERROR: Person Node fail.")
+    flaggedNodes<<-append(flaggedNodes, person)
+  }
+}) 
 
 
-# TrtArm : Check: "TrtArm" + attendeeNum+ "-"+ number1-4 (they may add arms)
+# TrtArm : Check: "TrtArm" + attendeeNum+ "-"+ number 
+armRegex <- paste0("eg:TrtArm", attendeeNum, "-", "\\d")
+sapply(armNodes, function(arm){
+  if (grepl(armRegex, arm)) {
+    print ("----Arm Pattern Match")
+  }else{
+    print ("----ERROR: Arm Node fail.")
+    flaggedNodes<<-append(flaggedNodes, arm)
+  }
+}) 
 
+# Add logic to test length() of vectors and display based on that.
+
+flaggedRelations
+
+flaggedNodes
 
 
 
