4c5bfc24628a4139a60fd2a6f0cac740beef5d45
NovasTaylor <NovasTaylor@gmail.com>
2018-02-19 19:37:15 -0500

    ping test script to confirm servers are reachable

---
+++ CSS2018/scripts/R/admin/PingServers.R
@@ -0,0 +1,25 @@
+###############################################################################
+# FILE: PingServers.R
+# DESC: Confirm workshop servers are up. 
+# IN  : ClassInfo.xlsx - has IP address of each study for each attendee
+#       ClassInfo.xlsx - Development version for testing
+# OUT : R console 
+# REQ : 
+# NOTE: 
+# TODO: 
+###############################################################################
+library(readxl)
+library(pingr)
+
+servers <- as.data.frame(read_excel("C:/_gitHub/LinkedDataWorkshop/CSS2018/data/ClassInfo-Dev.xlsx", sheet = "Servers", col_names=TRUE))
+
+for (i in 1:nrow(servers)){
+    pingResult<-ping(servers[i,2], count = 1, timeout = 1)
+    if (!is.na(pingResult)){
+      status<-"Pass"
+    }  else {
+      status<-"!----FAIL----!"
+    }
+    summary <- paste("Server" , i, " ", servers[i,2], ": ", status )
+    print(summary)
+}
