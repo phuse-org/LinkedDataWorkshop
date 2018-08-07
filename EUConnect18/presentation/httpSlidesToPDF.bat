a70745551042b4aac9b9990f321f2bf2256ee845
NovasTaylor <novastaylor@gmail.com>
2018-08-07 09:43:20 -0400

    Copy of files from CSS2018 as starting point for EUConnect18

---
+++ EUConnect18/presentation/httpSlidesToPDF.bat
@@ -0,0 +1,2 @@
+cd C:\_gitHub\LinkedDataWorkshop\CSS2018\presentation
+python -m http.server
---
+++ EUConnect18/scripts/R/admin/PingServers.R
@@ -0,0 +1,26 @@
+###############################################################################
+# FILE: PingServers.R
+# DESC: Confirm workshop servers are up. 
+# IN  : ClassInfo.xlsx - has IP address of each study for each attendee
+#       ClassInfo.xlsx - Development version for testing
+# OUT : R console 
+# REQ : 
+# NOTE: 
+# TODO: 
+###############################################################################
+library(readxl)
+library(pingr)
+
+servers <- as.data.frame(read_excel("C:/_gitHub/LinkedDataWorkshop/CSS2018/data/admin/ClassInfo-Dev.xlsx", sheet = "Servers", col_names=TRUE))
+
+for (i in 1:nrow(servers)){
+    ip <- gsub(" ", "",servers[i,2])
+    pingResult<-ping(ip, count = 1, timeout = 1)
+    if (!is.na(pingResult)){
+      status<-"Pass"
+    }  else {
+      status<-"!----FAIL----!"
+    }
+    summary <- paste("Server" , i, " ", servers[i,2], ": ", status )
+    print(summary)
+}
