503a5a63baba724514a02926e03e9218b7de5d81
NovasTaylor <novastaylor@gmail.com>
2019-10-27 15:04:20 -0400

    converted from redland R pkg to rdflib

--- EUConnect19/scripts/R/TTLValidation.R
+++ EUConnect19/scripts/R/TTLValidation.R
@@ -1,19 +1,19 @@
 ###############################################################################
 # FILE: /scripts/r/TTLValidation.R
-# DESC: LDWorkshop: Select, query, validate, and visuzalize a TTL file.
+# DESC: LDWorkshop: Select, query, validate, and visualize a TTL file.
 # IN  : 
 # OUT : N/A
 # REQ : 
 # NOTE: Visualize requires query to return s,p,o. 
-# 
-# TODO: 1. Reload of TTL does not reset QC Check data. Need to reset dataframes
-#       2. Convert from use of Redland to rdflib
+#       Valid NCT values are hard coded. Source: ClassInfo.xls. TODO: read direct
+#         from the XLS instead of hard code there.
+# TODO: 
 ###############################################################################
 library(DT)
 library(plyr)     #  rename
 library(reshape)  #  melt
 library(shiny)
-library(redland)
+library(rdflib)
 library(visNetwork)
 
 #---- Values to check ---------------------------------------------------------
@@ -102,37 +102,24 @@ WHERE{
 server <- function(input, output, session) {
 
     queryText <- observeEvent(input$fileRQ, {
-        filePath <- input$fileRQ$datapath
-        queryText <- paste(readLines(filePath), collapse = "\n")
-        # Update text area with file content
-        updateTextAreaInput(session, "query", value = queryText)
-        # return the text to be displayed in text Outputs
-        return(queryText)
+      filePath  <- input$fileRQ$datapath
+      queryText <- paste(readLines(filePath), collapse = "\n")
+      
+      # Update text area with file content
+      updateTextAreaInput(session, "query", value = queryText)
+      
+      return(queryText) # return the text to displayin text Outputs
     })
     output$query <- renderPrint({ queryText() })    
 
     data <- eventReactive(input$runQuery, {
-
-        # Setup the file read for redland
-        world   <- new("World")
-        storage <- new("Storage", world, "hashes", name="", options="hash-type='memory'")
-        model   <- new("Model", world=world, storage, options="")
-        parser  <- new("Parser", world, name = 'turtle', mimeType = 'text/turtle')
-
-        inFileTTL <- input$fileTTL
-            redland::parseFileIntoModel(parser, world, inFileTTL$datapath, model)
-       
-            queryResults = c(); 
-            query <- new("Query", world, input$query, base_uri=NULL, query_language="sparql", query_uri=NULL)
-            queryResult <- executeQuery(query, model)
-            
-            # getNextResult in a loop until NULL is returned.
-            repeat{
-                nextResult <- getNextResult(queryResult)
-                queryResults <- rbind(queryResults, data.frame(nextResult))
-                if(is.null(nextResult)){ break }
-            }
-        triples<-queryResults
+      # Reset QC Result dataframe
+      qcObs <<- data.frame(Examine = character(), 
+                           Value   = character(), 
+                           Message = character())
+      inFileTTL <- input$fileTTL
+      rdf <- rdf_parse(inFileTTL$datapath, format = 'turtle')
+      triples <- rdf_query(rdf, input$query)
     })
 
     # Query Result
@@ -142,35 +129,31 @@ server <- function(input, output, session) {
     #   Massage data for both QC Check and Visualization
     #   prefData = prefixes instead of IRIs
     prefData = reactive ({
-        # Replace IRI with prefixes for both plotting and data QC
-        toPref <- as.data.frame(data())
+      # Replace IRI with prefixes for both plotting and data QC
+      toPref <- as.data.frame(data())
         
-        # Convert IRI to use of prefixes
-        iriToPref <- function(elem)
-        {
+      # Convert IRI to use of prefixes
+      iriToPref <- function(elem)
+      {
+        # eg:
+        elem <- gsub("http://example.org/LDWorkshop#", "eg:", elem)
             
-            # eg:
-            elem <- gsub("<http://example.org/LDWorkshop#", "eg:", elem)
+        # ncit:
+        elem <- gsub("http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#", "ncit:",elem)
             
-            # ncit:
-            elem <- gsub("<http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#", "ncit:",elem)
+        # schema:
+        elem <- gsub("http://schema.org/", "schema:", elem)
             
-            # schema:
-            elem <- gsub("<http://schema.org/", "schema:", elem)
+        # ct:
+        elem <- gsub("http://bio2rdf.org/clinicaltrials:", "ct:", elem)
             
-            # ct:
-            elem <- gsub("<http://bio2rdf.org/clinicaltrials:", "ct:", elem)
+        #dbpedia
+        elem <- gsub("http://dbpedia.org/resource/", "dbpedia:", elem)
             
-            #dbpedia
-            elem <- gsub("<http://dbpedia.org/resource/", "dbpedia:", elem)
-            
-            # Remove the trailing >
-            elem <- gsub(">", "", elem)
-            
-            # Object literals require removal of quotes and type to get value only
-            elem <- gsub("^\"", "", elem)  # quote at start of value
-            elem <- gsub("\"\\S+", "", elem)  # quote value end and type
-        }  
+        # Object literals require removal of quotes and type to get value only
+        elem <- gsub("^\"", "", elem)  # quote at start of value
+        elem <- gsub("\"\\S+", "", elem)  # quote value end and type
+      }  
         toPref$s <- iriToPref(toPref$s) # Subjects
         toPref$p <- iriToPref(toPref$p) # Predicates
         toPref$o <- iriToPref(toPref$o) # Objects
@@ -184,12 +167,11 @@ server <- function(input, output, session) {
                         Value   = character(), 
                         Message = character())
 
-
     #--------------------------------------------------------------------------
     #---- Data QC -------------------------------------------------------------
     
     qcData  = reactive ({
-        
+      
        #---- Nodes -----------------------------------------------------------
        nodeList <- melt(prefData(), id.vars=c("p"))
        nodes <- nodeList$value
@@ -340,12 +322,11 @@ server <- function(input, output, session) {
          }
        }
        
-       
       #--- END OF CHECKS  -----------------------------------------------------
        
       # If no QC findings added to dataset, then add a row stating all checks passed. 
       if (dim(qcObs)[1] == 0) {
-        qcCurrVal <- c("", "All QC Checks Passed")
+        qcCurrVal <- c("", " ", "All QC Checks Passed")
         qcObs <- rbind(qcObs, qcCurrVal)
       }
       else{
@@ -366,85 +347,86 @@ server <- function(input, output, session) {
 
     #--------------------------------------------------------------------------
     #-- Visualize -------------------------------------------------------------
+    
     output$network <- renderVisNetwork({
-        RDFTriples <<- as.data.frame(prefData())
+      RDFTriples <<- as.data.frame(prefData())
         
-        RDFTriples<-RDFTriples[!(RDFTriples$o==""),]
+      RDFTriples<-RDFTriples[!(RDFTriples$o==""),]
         
-        # Remove duplicates from the query
-        RDFTriples <- RDFTriples[!duplicated(RDFTriples),]
+      # Remove duplicates from the query
+      RDFTriples <- RDFTriples[!duplicated(RDFTriples),]
 
-        #---- Nodes Construction
-        # Get the unique list of nodes by combine Subject and Object into 
-        # single column.
-        # "id.vars" = list of columns to keep untouched whil the unamed (s,o) are 
-        # melted into the "value" column.
-        nodeList <- melt(RDFTriples, id.vars=c("p" ))
+      #---- Nodes Construction
+      # Get the unique list of nodes by combine Subject and Object into 
+      # single column.
+      # "id.vars" = list of columns to keep untouched whil the unamed (s,o) are 
+      # melted into the "value" column.
+      nodeList <- melt(RDFTriples, id.vars=c("p" ))
 
-        # A node can be both a Subject and a Predicate so ensure a unique list of node names
-        #  by dropping duplicate values.
-        nodeList <- nodeList[!duplicated(nodeList$value),]
+      # A node can be both a Subject and a Predicate so ensure a unique list of node names
+      #  by dropping duplicate values.
+      nodeList <- nodeList[!duplicated(nodeList$value),]
 
-        # Rename to ID for use in visNetwork and keep only that column
-        nodeList <- rename(nodeList, c("value" = "id" ))
-        nodes<- as.data.frame(nodeList[c("id")])
+      # Rename to ID for use in visNetwork and keep only that column
+      nodeList <- rename(nodeList, c("value" = "id" ))
+      nodes<- as.data.frame(nodeList[c("id")])
 
-        # Assign node color based on content (int, string) then based on prefixes
-        nodes$group <- 'iri'
-        nodes$group[grepl("^\\w+", nodes$id, perl=TRUE)]         <- "string"
-        nodes$group[grepl("^\\d+", nodes$id, perl=TRUE)]         <- "int"
-        nodes$group[grepl("ncit:|schema:", nodes$id, perl=TRUE)] <- "iriont"
-        nodes$group[grepl("ct:|dbpedia:", nodes$id, perl=TRUE)]  <- "iriext"
-        nodes$group[grepl("eg:", nodes$id, perl=TRUE)]           <- "iri"
-        
-        nodes$shape <- "box"
-        nodes$title <-  nodes$id  # mouseover. 
-        nodes$label <- nodes$title # label on node (always displayed)
+      # Assign node color based on content (int, string) then based on prefixes
+      nodes$group <- 'iri'
+      nodes$group[grepl("^\\w+", nodes$id, perl=TRUE)]         <- "string"
+      nodes$group[grepl("^\\d+", nodes$id, perl=TRUE)]         <- "int"
+      nodes$group[grepl("ncit:|schema:", nodes$id, perl=TRUE)] <- "iriont"
+      nodes$group[grepl("ct:|dbpedia:", nodes$id, perl=TRUE)]  <- "iriext"
+      nodes$group[grepl("eg:", nodes$id, perl=TRUE)]           <- "iri"
+      
+      nodes$shape <- "box"
+      nodes$title <-  nodes$id  # mouseover. 
+      nodes$label <- nodes$title # label on node (always displayed)
 
-        
-        #---- Edges
-        # Create list of edges with from, to for visNetwork 
-        edges<-as.data.frame(rename(RDFTriples, c("s" = "from", "o" = "to")))
-        
-        # Edge values
-        #   edges$label : always displayed, so not set in current vis.
-        #   edges$title : only displayed on mouseover. Used in current vis.
-        edges$title <- edges$p
+      
+      #---- Edges
+      # Create list of edges with from, to for visNetwork 
+      edges<-as.data.frame(rename(RDFTriples, c("s" = "from", "o" = "to")))
+      
+      # Edge values
+      #   edges$label : always displayed, so not set in current vis.
+      #   edges$title : only displayed on mouseover. Used in current vis.
+      edges$title <- edges$p
 
-        visNetwork(nodes, edges, height = "1200px", width = "100%") %>%
-            visOptions(selectedBy = "group", 
-                highlightNearest = TRUE, 
-                nodesIdSelection = TRUE) %>%
-            visEdges(arrows = list(to = list(enabled = TRUE, scaleFactor = 0.5)),
-                     color  = "gray",
-                     smooth = list(enabled = FALSE, type = "cubicBezier", roundness=.8)) %>%
-            visGroups(groupname = "iri",    color = list(background = "#BCF5BC", 
-                                                         border     = "#CCCCCC",
-                                                         highlight  = "#FFFF33")) %>%
+      visNetwork(nodes, edges, height = "1200px", width = "100%") %>%
+        visOptions(selectedBy = "group", 
+             highlightNearest = TRUE, 
+             nodesIdSelection = TRUE) %>%
+        visEdges(arrows = list(to = list(enabled = TRUE, scaleFactor = 0.5)),
+                 color  = "gray",
+                 smooth = list(enabled = FALSE, type = "cubicBezier", roundness=.8)) %>%
+        visGroups(groupname = "iri",    color = list(background = "#BCF5BC", 
+                                                     border     = "#CCCCCC",
+                                                     highlight  = "#FFFF33")) %>%
 
-          visGroups(groupname = "iriext", color = list(background = "#BCBDDC",
-                                                       border     = "#CCCCCC", 
-                                                       highlight  = "#FFFF33")) %>%
+        visGroups(groupname = "iriext", color = list(background = "#BCBDDC",
+                                                     border     = "#CCCCCC", 
+                                                     highlight  = "#FFFF33")) %>%
           
           
-            visGroups(groupname = "iriont", color = list(background = "#FFC862",
-                                                         border     = "#CCCCCC", 
-                                                         highlight  = "#FFFF33")) %>%
+        visGroups(groupname = "iriont", color = list(background = "#FFC862",
+                                                     border     = "#CCCCCC", 
+                                                     highlight  = "#FFFF33")) %>%
 
-            visGroups(groupname = "string", color = list(background = "#E4E4E4", 
-                                                         border     = "#CCCCCC", 
-                                                         highlight  = "#FFFF33")) %>%
-            visGroups(groupname = "int",    color = list(background = "#C1E1EC", 
-                                                         border     = "#CCCCCC",
-                                                         highlight  = "#FFFF33" )) %>%
-            visPhysics(stabilization=FALSE, 
+        visGroups(groupname = "string", color = list(background = "#E4E4E4", 
+                                                     border     = "#CCCCCC", 
+                                                     highlight  = "#FFFF33")) %>%
+        visGroups(groupname = "int",    color = list(background = "#C1E1EC", 
+                                                     border     = "#CCCCCC",
+                                                     highlight  = "#FFFF33" )) %>%
+        visPhysics(stabilization = FALSE, 
                 barnesHut = list(
-                    avoidOverlap=1,
-                    gravitationalConstant = -2000,
-                    springConstant = 0.01,
-                    damping = 0.2,
-                    springLength = 50
-            ))  
+                  avoidOverlap          = 1,
+                  gravitationalConstant = -2000,
+                  springConstant        = 0.01,
+                  damping               = 0.2,
+                  springLength          = 50
+        ))  
     })
 }
 shinyApp(ui = ui, server = server)
